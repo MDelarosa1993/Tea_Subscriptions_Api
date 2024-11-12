dc8053505f65a66cc7eee5026483da61d2bfbf08
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-11-12 16:03:56 -0500

    Added validations to my models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,9 @@ class Subscription < ApplicationRecord
   belongs_to :customer
   has_many :subscription_teas
   has_many :teas, through: :subscription_teas
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :status, presence: true, inclusion: { in: %w[active cancelled] }
+  validates :frequency, presence: true, inclusion: { in: %w[weekly monthly yearly] }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Subscription, type: :model do
   it { should belong_to(:customer) }
   it { should have_many(:subscription_teas)}
   it { should have_many(:teas).through(:subscription_teas) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price).is_greater_than(0) }
+  it { should validate_presence_of(:status) }
+  it { should validate_inclusion_of(:status).in_array(%w[active cancelled]) }
+  it { should validate_presence_of(:frequency) }
+  it { should validate_inclusion_of(:frequency).in_array(%w[weekly monthly yearly]) }
 end
